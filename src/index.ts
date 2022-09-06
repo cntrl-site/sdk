d162d84182a524df3bab8dc65d29788d3eee4433
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-09-06 11:51:15 +0300

    export all core

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Client as CntrlClient } from './Client/Client';
 export { FontFaceGenerator } from './FontFaceGenerator/FontFaceGenerator';
 export { getLayoutStyles } from './utils';
+export * from '@cntrl-site/core';
