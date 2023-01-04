53ebb56d5b266a8bad12ee970f137582d1c58db2
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2023-01-04 17:48:37 +0200

    CNTRL-737 Type presets and tests for Client

---
+++ src/Client/__mock__/articleMock.ts
@@ -0,0 +1,15 @@
+import { TArticle } from '@cntrl-site/core';
+
+export const articleMock: TArticle = {
+  id: 'articleId',
+  sections: [
+    {
+      id: 'sectionId',
+      items: [],
+      position: {},
+      height: {},
+      hidden: {},
+      color: {}
+    }
+  ]
+};
